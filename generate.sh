0358052ba97f6a1223f3615c7f033c1165297a17
Kim K <koomen@protonmail.com>
2017-01-21 13:54:17 +0100

    added generate test file script

---
+++ generate.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#
+# generate.sh
+# Copyright (C) 2017 Kim Koomen <koomen@protonail.com>
+#
+# Distributed under terms of the MIT license.
+#
+#
+# Run this file to create test files. These are most of the cases we can expect.
+
+touch "FOx sTeVENsoN - trIGgeR.mp3"
+touch "FOx sTeVENsoN - trIGgeR.mp3"
+touch "FOx sTeVENsoN- trIGgeR.mp3"
+touch "FOx sTeVENsoN -trIGgeR.mp3"
+touch "FOx sTeVENsoN-trIGgeR.mp3"
+touch "Fox   sTeVENsoN     -       trIGgeR.mp3"
+touch "01.FOx sTeVENsoN - trIGgeR.mp3"
+touch "01.FOx sTeVENsoN- trIGgeR.mp3"
+touch "01.FOx sTeVENsoN -trIGgeR.mp3"
+touch "01.FOx sTeVENsoN-trIGgeR.mp3"
+touch "01. FOx sTeVENsoN -trIGgeR.mp3"
+touch "01. FOx sTeVENsoN- trIGgeR.mp3"
+touch "01. FOx sTeVENsoN-trIGgeR.mp3"
+touch "01-FOx sTeVENsoN -trIGgeR.mp3"
+touch "01-FOx sTeVENsoN-trIGgeR.mp3"
+touch "01-FOx sTeVENsoN-trIGgeR.mp3"
+touch "01 - FOx sTeVENsoN -trIGgeR.mp3"
+touch "01 - FOx sTeVENsoN- trIGgeR.mp3"
+touch "01 - FOx sTeVENsoN-trIGgeR.mp3"
+touch "01 -FOx sTeVENsoN -trIGgeR.mp3"
+touch "01 -FOx sTeVENsoN- trIGgeR.mp3"
+touch "01 -FOx sTeVENsoN-trIGgeR.mp3"
+touch "01- FOx sTeVENsoN -trIGgeR.mp3"
+touch "01- FOx sTeVENsoN- trIGgeR.mp3"
+touch "01- FOx sTeVENsoN-trIGgeR.mp3"
+touch "01FOx sTeVENsoN -trIGgeR.mp3"
+touch "01FOx sTeVENsoN-trIGgeR.mp3"
+touch "01FOx sTeVENsoN-trIGgeR.mp3"
+touch "01 FOx sTeVENsoN -trIGgeR.mp3"
+touch "01 FOx sTeVENsoN- trIGgeR.mp3"
+touch "01 FOx sTeVENsoN-trIGgeR.mp3"
+touch "01)FOx sTeVENsoN -trIGgeR.mp3"
+touch "01)FOx sTeVENsoN-trIGgeR.mp3"
+touch "01)FOx sTeVENsoN-trIGgeR.mp3"
+touch "01 ) FOx sTeVENsoN -trIGgeR.mp3"
+touch "01 ) FOx sTeVENsoN- trIGgeR.mp3"
+touch "01 ) FOx sTeVENsoN-trIGgeR.mp3"
+touch "01 )FOx sTeVENsoN -trIGgeR.mp3"
+touch "01 )FOx sTeVENsoN- trIGgeR.mp3"
+touch "01 )FOx sTeVENsoN-trIGgeR.mp3"
+touch "01) FOx sTeVENsoN -trIGgeR.mp3"
+touch "01) FOx sTeVENsoN- trIGgeR.mp3"
+touch "01) FOx sTeVENsoN-trIGgeR.mp3"
